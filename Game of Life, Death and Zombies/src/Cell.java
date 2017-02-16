ea080cb6f72c278e891b772b7bf77fdd0a191cc6
JonathanXSG <jonathanx01228@gmail.com>
2017-02-16 00:12:37 -0400

    Created GameBoard and Cell toggling functionality

--- Game of Life, Death and Zombies/src/Cell.java
+++ Game of Life, Death and Zombies/src/Cell.java
@@ -2,10 +2,15 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 
 import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
+
+import javafx.scene.input.MouseButton;
 
 public class Cell extends JComponent{
 	private String status;
@@ -14,11 +19,45 @@ public class Cell extends JComponent{
 	int y;
 	int height = 20;
 	int width = 20;
+	Boolean state = false;
 	
 	public Cell(int x, int y, String status){
 		this.x=x;
 		this.y=y;
 		this.status=status;
+		
+		this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            	mouseClick(e);
+            }});
+	}
+	
+	public void mouseClick(MouseEvent e){
+		this.state= !state;
+    	if(e.isControlDown() || SwingUtilities.isRightMouseButton(e)){
+    		if(this.status != "zombie"){
+    			setStatus("zombie");
+    		}
+    		else if(this.status=="zombie"){
+    			if(this.state==true){
+        			setStatus("alive");
+        		}
+        		else{
+        			setStatus("daed");
+        		}
+    		}
+    	}
+    	else if(SwingUtilities.isLeftMouseButton(e)){
+			System.out.println("hello");
+    		if(state==true){
+    			setStatus("alive");
+    		}
+    		else{
+    			setStatus("daed");
+    		}
+    	}
+        repaint();
 	}
 	
 	public void setStatus(String status){
@@ -33,10 +72,13 @@ public class Cell extends JComponent{
 		if(status=="alive"){
 			g2.setColor(Color.BLUE);
 		}
-		else{
+		else if (status == "zombie"){
+			g2.setColor(Color.GREEN);
+		}
+		else if(status == "dead"){
 			g2.setColor(Color.BLACK);
 		}
-		g2.fillRect(x, y, width, height);;
+		g2.fillRect(0, 0, width, height);;
 	}
 	
 	@Override
@@ -45,8 +87,9 @@ public class Cell extends JComponent{
 	}
 	@Override
 	public void paintComponent(Graphics g){
+		this.draw(g);
 		super.paintComponent(g);
-
+		
 	}
 	
 	
--- Game of Life, Death and Zombies/src/GameBoard.java
+++ Game of Life, Death and Zombies/src/GameBoard.java
@@ -1,31 +1,42 @@
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.ComponentOrientation;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import javax.swing.*;
 
 public class GameBoard extends JPanel{
 	ArrayList<Cell> cells = new ArrayList<Cell>();
-	int xPadding=5;
-	int yPadding=5;
-
+	int hGap=1;
+	int vGap=1;
+	int cellsCount;
+	
 	public GameBoard() {
+		applyComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		setLayout(new GridLayout(0,10,hGap,vGap));
 		setBackground(Color.DARK_GRAY);
 		setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        
         for(int i=0;i<10;i++){
-			for(int j=0;j<10;j++){
-				cells.add(new Cell(j*25+xPadding,i*25+yPadding,"alive"));
+			for(int j=0;j<20;j++){
+				cells.add(new Cell(j,i,"alive"));
 			}
 		}
-        cells.get(2).setStatus("dead");
+        for(int  i=0;i<cells.size();i++){
+        	add(cells.get(i));
+        }
+        
     }
-
+	
     @Override
 	public Dimension getPreferredSize() {
 		  return new Dimension(500, 500);
@@ -33,8 +44,5 @@ public class GameBoard extends JPanel{
 	@Override
 	public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        for(Cell i:cells){
-	    	   i.draw(g);
-	    }
     }  
 }
